a58979dc00df75c770a329b9fc600be9b6bf46a0
unknown <sanketganorkar.sg@gmail.com>
2023-11-26 20:00:33 +0530

    Project

---
+++ config.py
@@ -0,0 +1,11 @@
+
+import os
+
+
+CSV_FILE_PATH = "enter_path"
+
+
+
+MODEL_FILE_PATH = os.path.join("project_app", "Linear_model.pkl")
+
+JSON_FILE_PATH = os.path.join("project_app", "project_data.json")
